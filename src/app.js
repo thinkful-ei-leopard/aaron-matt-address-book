5b62afe0bc6f4a45f9af665383cad0093f02d6af
AveraqeDev <averaqedev@gmail.com>
2020-02-20 14:24:19 -0800

    fixes

--- src/app.js
+++ src/app.js
@@ -109,13 +109,13 @@ app.use((error, req, res, next) => {
 });
 
 function validateToken (req, res, next) {
-  const apiToken = API_TOKEN
-  const authToken = req.get('Authorization')
+  const apiToken = API_TOKEN;
+  const authToken = req.get('Authorization');
 
   if (!authToken || authToken.split(' ')[1] !==apiToken) {
-      return res.status(401).json({ error: 'Unauthorized request'})
+    return res.status(401).json({ error: 'Unauthorized request'});
   }
-  next()
+  next();
 }
 
 module.exports = app;
